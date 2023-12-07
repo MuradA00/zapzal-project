50e7db7f1f65cb685681e29c91eec51d0d1d32b0
MuradA00 <murad.abyshly25@gmail.com>
2023-12-08 01:56:03 +0200

    90%

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,21 @@
 const themeItems = document.querySelectorAll('.header-theme__item');
 const darkThemeButton = document.querySelector('.header-theme__item--sun');
 const sunThemeButton = document.querySelector('.header-theme__item--moon');
+const headerMenu = document.querySelector('.header-menu');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('.header')
+
+headerMenu.addEventListener('click', () => {
+  headerMenu.classList.toggle('header-menu--active');
+
+  if (headerMenu.classList.contains('header-menu--active')) {
+    header.classList.add('header--fixed');
+    menu.classList.add('menu--active')
+  } else {
+    header.classList.remove('header--fixed')
+    menu.classList.remove('menu--active')
+  }
+})
 
 const toggleTheme = () => {
   document.documentElement.classList.toggle('dark-theme');
